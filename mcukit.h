f98f66f9ac9bbe502c6ce30b9179dfad44b2c2f7
luisf <dropmeaword@gmail.com>
2022-11-30 09:17:13 +0100

    reorganized to make it a valid PlatformIO library

---
+++ mcukit.h
@@ -0,0 +1,13 @@
+#ifndef __MCUKIT_H__
+#define __MCUKIT_H__
+
+
+#include "corestats/corestats.h"
+#include "derfunke/derfunke.h"
+#include "easywifi/easywifi.h"
+#include "nanolog/nanolog.h"
+#include "ota/ota.h"
+#include "smoothie/smoothie.h"
+#include "ugen/ugen.h"
+
+#endif // __MCUKIT_H__
